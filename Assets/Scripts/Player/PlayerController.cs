3dff2f6e97ff19c429e1283d6d9cbca673adb8a1
Pau <pauguvi@gmail.com>
2023-07-22 21:10:53 +0200

    Exterior Camera Setup

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+    public float horizontalMaxSpeed = 5f;
+    public float horizontalAccelTime = 0.1f;
+    public float horizontalDecelTime = 0.1f;
+    public float rotationSpeed = 20f;
+
+    public bool canPush = true;
+
+    [HideInInspector] public Vector2 horizontalInputValue;
+    [HideInInspector] public Vector2 horizontalDirection;
+    [HideInInspector] public Vector2 horizontalSpeed = Vector2.zero;
+    [HideInInspector] public Vector2 horizontalAccel;
+    [HideInInspector] public Vector3 externalForces = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        SetHorizontalSpeed();
+        Move();
+        Rotate(horizontalDirection);
+    }
+
+    public void OnMove(InputAction.CallbackContext value)
+    {
+        horizontalInputValue = value.ReadValue<Vector2>();
+    }
+
+    public void OnAction(InputAction.CallbackContext value)
+    {
+
+    }
+
+    void SetHorizontalSpeed()
+    {
+        if (horizontalInputValue.magnitude > 0f)
+        {
+            horizontalDirection = horizontalInputValue;
+            horizontalSpeed = Vector2.SmoothDamp(horizontalSpeed, horizontalDirection * horizontalMaxSpeed, ref horizontalAccel, horizontalAccelTime);
+        }
+        else
+        {
+            horizontalSpeed = Vector2.SmoothDamp(horizontalSpeed, Vector2.zero, ref horizontalAccel, horizontalDecelTime);
+            if (horizontalSpeed.magnitude < 0.1f)
+            {
+                horizontalSpeed = Vector3.zero;
+            }
+        }
+    }
+
+    void Move()
+    {
+        // aplying movement
+        Vector3 motion = new Vector3(horizontalSpeed.x * Time.deltaTime, 0, horizontalSpeed.y * Time.deltaTime);
+        characterController.Move(motion);
+    }
+
+    void Rotate(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            Quaternion facingAngle = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y));
+            transform.rotation = Quaternion.Lerp(transform.rotation, facingAngle, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
